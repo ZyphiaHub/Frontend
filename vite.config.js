2896bad68e98cf76a144a3e7703afc7c93025181
ZyphiaHub <149183538+ZyphiaHub@users.noreply.github.com>
2024-11-04 15:37:15 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     plugins: [sveltekit()],
-    base: '/myproject/',
+    base: '/Frontend/',
     build: {
         outDir: 'docs',
         emptyOutDir: true
