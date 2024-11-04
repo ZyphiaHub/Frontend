600892a3426fc24306c2673be14826d909a9dddf
ZyphiaHub <149183538+ZyphiaHub@users.noreply.github.com>
2024-11-04 12:38:49 +0100

    upd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,18 @@
 <h1>Frontend projects</h1>
 
-    <p>
+<div class="linklada">
+    <a href="https://example.com" class="button">Receptkönyv</a>
+</div>
+<p>
         <a target="_blank" href="https://github.com/ZyphiaHub/Frontend.git">
             <img src="https://cdn.iconscout.com/icon/free/png-256/free-github-169-1174970.png" alt="GitHub"></a>
-    </p>
+</p>
     
 <style>
     h1 {
+        color: aliceblue;
         font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
-        background-color: blanchedalmond;
+        background-color: #435d7a;
         text-align: center;
         }
     a {
@@ -21,9 +25,29 @@
     img {
         
         max-width: 50px;
-        border: 1px;
-        border-color: black;
+        
     }
+    .button {
+  display: inline-block;
+  padding: 10px 20px;
+  background-color: #435d7a;
+  color: white;
+  text-decoration: none;
+  border-radius: 5px;
+  font-size: 16px;
+  text-align: center;
+}
+.button:hover {
+  background-color: #93b7de;
+}
+.linklada {
+    align-content: space-around;
+    display: flex;
+    gap: 10px; /* Térköz a gombok között */
+    justify-content: center; /* Igazítás középre */
+    flex-wrap: wrap; /* Törés, ha nem férnek el egymás mellett */
+}
+
     
 </style>
 
